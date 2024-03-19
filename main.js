45a8cd7c1ef1de5b5db9d4de59c30058db15aaea
dikshabhide <162450819+dikshabhide@users.noreply.github.com>
2024-03-20 01:21:24 +0530

    Add files via upload
    some changes panding

---
+++ main.js
@@ -0,0 +1,31 @@
+/*================toggle icon navbar==============*/
+
+
+
+let  menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('navbar');
+
+menuIcon.onclick=() =>{
+    menuIcon.classList.toggle('fa-xmark');
+navbar.classList,toggle('active');
+}
+/*================ scroll down active link==============*/
+let sections =document.querySelectorAll('section');
+let navLinks = document.querySelectorAll ('header nav a');
+
+window.onscroll= ()=>{
+    sections.forEach(sec =>{
+        let top = window.screenY;
+        let offset =sec.offsetTop-150;
+          let  height= sec.offsetHeight;
+          let id= sec.getAttribute('id');
+          if(top >= offset && top < offset + height)
+{
+       navLinks.forEach.apply(links => {
+        links.classlist.removeb('active');
+      document.querySelector('header nav a [href*='+id+']').classList.add('active');
+
+       }) ;
+} ;         
+    });
+}
